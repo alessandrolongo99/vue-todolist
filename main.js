e8a5659c6ca4736f80bf5147242125eaa4707fd3
alessandrolongo99 <longoalessandro153@gmail.com>
2022-07-06 12:55:19 +0200

    milestone 1

---
+++ main.js
@@ -0,0 +1,20 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        index: 0,
+        todos: [
+            {
+                text: 'Fare i compiti',
+                done: false
+            },
+            {
+                text: 'Fare la spesa',
+                done: true
+            },
+            {
+                text: 'Fare il bucato',
+                done: false
+            }
+        ]
+    },
+})
